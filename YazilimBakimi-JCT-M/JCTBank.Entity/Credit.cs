fd5671a19afddb81d20281fbe879820457cab437
Selennurdlsz <selennurdilsiz67@gmail.com>
2019-12-17 19:39:28 +0300

    Update

---
+++ YazilimBakimi-JCT-M/JCTBank.Entity/Credit.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JCTBank.Entity
+{
+    [Table("Credit")]
+    public class Credit
+    {
+        [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        public int LoanAmount { get; set; }
+
+        public int CustomerId { get; set; }
+        public virtual Customer Customer { get; set; }
+    }
+}
